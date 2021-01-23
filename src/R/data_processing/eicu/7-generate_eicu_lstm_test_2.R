f2d2611f3dfd0c10079b2ee69b384c8f49bf509e
Ran Liu <xabsox@gmail.com>
2021-01-23 11:44:06 -0500

    Generate reference files from eICU data

---
+++ src/R/data_processing/eicu/7-generate_eicu_lstm_test_2.R
@@ -0,0 +1,53 @@
+rm(list=ls())
+
+library(pracma)
+library(tictoc)
+library(glmnet)
+library(ROCR)
+library(matrixStats)
+library(parallel)
+library(ggplot2)
+library(xgboost)
+library(lubridate)
+
+eicu.clinical.data = readRDS("eicu/clinical_data_icd9_sofa_vent.rds")
+eicu.patient.result = readRDS("eicu/patient_data.rds")
+eicu.sofa.scores = readRDS("eicu/sofa_scores.rds")
+
+source("eicu_functions/generate_sampling_rate_table.R")
+source("eicu_functions/eval_carry_forward.R")
+source("eicu_functions/eval_interval.R")
+source("eicu_functions/eval_max_in_past_2.R")
+source("eicu_functions/eval_sum_in_past.R")
+source("eicu_functions/eval_early_prediction_timestamps_combined_rf.R")
+source("eicu_functions/eval_table_with_sofa.R")
+source("eicu_functions/generate_table_with_sofa_timestamps.R")
+source("eicu_functions/eval_table_with_sofa_timestamps_history.R")
+
+load("eicu.reference.rdata")
+
+lengths = sapply(eicu.sofa.scores, function(x) length(x$timestamps))
+eicu.sepsis.labels = sapply(eicu.sofa.scores[lengths>0], function(x) rowSums(x[2:7])>=2)
+eicu.has.sepsis = sapply(eicu.sepsis.labels, any)
+
+# Determine shock onsets
+eicu.shock.labels = mapply(function(x,y) x&y$lactate&y$vasopressors, eicu.sepsis.labels, eicu.sofa.scores[lengths>0])
+eicu.has.shock = sapply(eicu.shock.labels, function(x) any(x,na.rm=T)) #& has.dx[lengths>0]
+
+sepsis.label.lengths = sapply(eicu.sepsis.labels,length)
+shock.lengths=sapply(eicu.shock.labels,length)
+
+eicu.shock.onsets = mapply(function(x,y) min(x$timestamps[y],na.rm=T),eicu.sofa.scores[lengths>0][eicu.has.shock],eicu.shock.labels[eicu.has.shock])
+
+num.cores = 4
+cluster = makeCluster(num.cores)
+clusterExport(cluster,c("eval.table.with.sofa.timestamps.history","eval.carry.forward","eval.sum.in.past","eval.max.in.past","eval.interval","eicu.sofa.scores","eicu.clinical.data","eicu.shock.onsets","lengths","eicu.has.sepsis","eicu.has.shock"))
+clusterEvalQ(cluster,library(lubridate))
+tic("Generate data tables (parallel)")
+nonsepsis.data = parLapply(cluster, 1:sum(!eicu.has.sepsis), function(x) eval.table.with.sofa.timestamps.history(eicu.sofa.scores[lengths>0][!eicu.has.sepsis][[x]]$timestamps,eicu.clinical.data[lengths>0][!eicu.has.sepsis][[x]],hours(1),12))
+nonshock.data = parLapply(cluster, 1:sum(eicu.has.sepsis&!eicu.has.shock), function(x) eval.table.with.sofa.timestamps.history(eicu.sofa.scores[lengths>0][eicu.has.sepsis&!eicu.has.shock][[x]]$timestamps,eicu.clinical.data[lengths>0][eicu.has.sepsis&!eicu.has.shock][[x]],hours(1),12))
+preshock.data = parLapply(cluster, 1:sum(eicu.has.shock), function(x) eval.table.with.sofa.timestamps.history(eicu.sofa.scores[lengths>0][eicu.has.shock][[x]]$timestamps[eicu.sofa.scores[lengths>0][eicu.has.shock][[x]]$timestamps<=eicu.shock.onsets[x]],eicu.clinical.data[lengths>0][eicu.has.shock][[x]],hours(1),12))
+toc()
+stopCluster(cluster)
+
+save(nonsepsis.data,nonshock.data,preshock.data,file="eicu.lstm.test.rdata")
